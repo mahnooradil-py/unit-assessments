8763f8c9ad7fade76a7930acab8c1a82f765a785
mahnooradil-py <mahnooradil2001@gmail.com>
2024-12-06 12:23:56 +0000

    Data Structures (Lists)

---
+++ listops.py
@@ -0,0 +1,16 @@
+the_num_list=[0,20,30,42,56,70,200,250,600,1000]
+
+def n_list(num):
+    print("The list:",list(num))
+def n_sum(num):
+    print("The total is:",sum(num))
+def n_l(num):
+    print("The largest:",max(num))
+def n_r(num):
+    print("The list in reversed:",reverse(num))
+def n_sm(num):
+    print("The smallest:",min(num))
+    
+
+
+
