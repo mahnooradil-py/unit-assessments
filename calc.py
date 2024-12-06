1c933454036e28cd2cc667b39b2d12804efea00e
mahnooradil-py <mahnooradil2001@gmail.com>
2024-12-06 12:12:07 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+sum_result=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+
+print(sum_result)
+
+
