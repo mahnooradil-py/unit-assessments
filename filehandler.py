44c267446cb34160207850d424069ede929e7f4b
mahnooradil-py <mahnooradil2001@gmail.com>
2024-12-13 15:04:47 +0000

    Add files via upload

---
+++ filehandler.py
@@ -0,0 +1,15 @@
+def calculate_grade(mark):
+    if mark >= 70:
+        return "Distinction"
+    elif mark >= 60:
+        return "Merit"
+    elif mark >= 50:
+        return "Pass"
+    else:
+        return "Fail"
+    def read_input_file(filename):
+    students = {}
+    with open(filename, 'r') as file:
+    
+    
+
