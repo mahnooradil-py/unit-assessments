c145446345b8b718b572a2d37b1c7c94955f7447
mahnooradil-py <mahnooradil2001@gmail.com>
2024-12-13 15:00:46 +0000

    loops.py

--- loops.py
+++ loops.py
@@ -1,6 +1,5 @@
 n=int(input("Enter a non-negative integer: "))
 factorial=1
 for i in range(1,n+1):
-    factorial=factorial *i
-    print(i,":",factorial)
-    
+    factorial *= i
+    print(f"The factorial of {n}is: {factorial}")
